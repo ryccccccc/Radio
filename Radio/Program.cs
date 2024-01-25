c722fd284dc028c8c64855e63e2937834c11b26e
Riccardo Nervosi <riccardo.nervosi@gmail.com>
2024-01-25 22:54:17 +0100

    Aggiungere i file di progetto.

---
+++ Radio/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Radio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Radio r = new Radio();
+            r.Volume = 100;
+
+            r.AumentaVolume();
+            Console.WriteLine(r.Volume);
+
+            r.Volume = 100;
+
+            r.DiminuisciVolume();
+            Console.WriteLine(r.Volume);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Radio/Radio.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Radio
+{
+    internal class Radio
+    {
+        public float Volume { get; set; }
+        public void AumentaVolume()
+        {
+            Volume = Volume + (0.1f * Volume);
+        }
+        public void DiminuisciVolume()
+        {
+            Volume = Volume - (0.1f * Volume);
+        }
+    }
+}
